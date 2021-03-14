96be94dbfbf0fdd5d62d0ba43aec4a65a4c6a564
amberteng407 <76562864+amberteng407@users.noreply.github.com>
2021-03-14 12:32:33 +0800

    Add files via upload

--- get-data.js
+++ get-data.js
@@ -7,28 +7,30 @@ function getData(){
             // JSON.parse是string轉object,方便撈取資料
             // xhr.responseText 獲得字符串形式的響應數據
             for (var i = 0; i < 8; i ++){
-                //放入文字
+                
                 let content = document.querySelector(".content");
                 let box = document.createElement("div");
                 box.className = "box";
                 box.id = "pic";
                 content.appendChild(box);
 
+                //載入照片放入 <div class="box" id = "pic"> 中       
+                let pic = document.createElement("div");
+                pic.className="img";
+                let pics = document.createElement("img");
+                pic.appendChild(pics);
+                let src1 = state["result"]["results"][i]["file"];
+                let src2 = src1.split("http:")[1]; 
+                pics.src = "http:" + src2;
+                box.appendChild(pic);
+
+                //放入文字
                 let word = document.createElement("div"); 
                 word.className = "word";
                 let name = state["result"]["results"][i]["stitle"];
                 let title = document.createTextNode(name);       
                 word.appendChild(title);
                 box.appendChild(word);
-                
-                //載入照片放入 <div class="box" id = "pic"> 中       
-                let pic = document.createElement("img");
-                pic.className="img";
-
-                let src1 = state["result"]["results"][i]["file"];
-                let src2 = src1.split("http:")[1]; 
-                pic.src2 = "http:" + src2;
-                box.appendChild(pic);
             }                     
         }
 }
